d8a26cf51e990625ea9f446d1f3cc98f4618feed
Crewmasta <41262507+Crewmasta@users.noreply.github.com>
2018-08-10 21:30:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,21 +14,7 @@ client.on('message', msg => {
 
 
 
-client.on('message', message => {
-  if (!message.guild) return;
 
-  if (message.content === '.join') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => {  
-          message.reply('I have successfully connected to the channel! <:anametnakkmd:477201848575590421>');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first! <:hm:477202287027290113>');
-    }
-  }
-});
 
 client.on('ready', () => {
   client.user.setGame(`JUST MORIKA JUST MORIKA , HEJ HEJ MORIKA HEJ HEJ MORIKA`,'https://www.twitch.tv/v5bz');
